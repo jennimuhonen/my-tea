b8d5cccec576c428c85f91d8b22d328603a673ac
Jenni Muhonen <145614394+jennimuhonen@users.noreply.github.com>
2026-04-17 18:36:58 +0300

    feat: minun teeni -kortit
    haetaan teet Firebasesta ja näytetään ne käyttäjälle kortteina + lisätty App.js paperprovider

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { PaperProvider } from 'react-native-paper';
 import Home from './Home';
 import AddTea from './AddTea';
 import MyTea from './MyTea';
@@ -14,12 +15,14 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Koti" component={Home} />
-        <Tab.Screen name="Minun teeni" component={MyTea} />
-        <Tab.Screen name="Lisää tee" component={AddTea} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Koti" component={Home} />
+          <Tab.Screen name="Minun teeni" component={MyTea} />
+          <Tab.Screen name="Lisää tee" component={AddTea} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- MyTea.js
+++ MyTea.js
@@ -1,17 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Card } from 'react-native-paper';
 import { app } from './firebaseConfig';
-import { getDatabase } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useState, useEffect } from 'react';
 
 //Lähteet: Kurssimateriaali
 
 const db = getDatabase(app);
 
 export default function MyTea() {
+
+  const [teas, setTeas] = useState([]);
+
+  useEffect(() => {
+    const itemsRef = ref(db, 'items/');
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const itemsArray = Object.entries(data).map(([key, value]) => ({
+          id: key,
+          ...value
+        }));
+        setTeas(itemsArray);
+        console.log(itemsArray)
+      } else {
+        setTeas([]);
+      }
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList
+        style={styles.list}
+        data={teas}
+        renderItem={({ item }) =>
+          <Card style={styles.card}>
+            <Card.Title title={item.name} />
+            <Card.Content>
+              <Text variant='bodyMedium'>Paikka: {item.place}</Text>
+            </Card.Content>
+          </Card>
+        }
+      />
     </View>
   );
 }
@@ -23,4 +55,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list: {
+    width: '100%',
+  },
+  card: {
+    marginBottom: 5,
+  }
 });
