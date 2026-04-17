622f111f65c2ede1415b15829e84861a80b0884e
Jenni Muhonen <145614394+jennimuhonen@users.noreply.github.com>
2026-04-17 16:07:29 +0300

    feat: Firebasen käyttöönotto
    Firebasen asennus ja käyttö addTea-lomakkeella

--- addTea.js
+++ addTea.js
@@ -1,28 +1,51 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Keyboard, Alert } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import { useState } from 'react';
+import { app } from './firebaseConfig';
+import { getDatabase, ref, push } from 'firebase/database';
+
+//Lähteet: Kurssimateriaali, ChatGPT (tarkemmin käytöstä koodissa)
+
+const db = getDatabase(app);
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [place, setPlace] = useState("");
+  const [tea, setTea] = useState({
+    name: '',
+    place: ''
+  });
+
+  const saveItem = () => {
+    if (tea.name && tea.place) {
+      push(ref(db, 'items/'), tea);
+      setTea({
+        name: '',
+        place: ''
+      })
+      Keyboard.dismiss(); //Lähde: ChatGPT
+    } else {
+      Alert.alert('Tallennus ei onnistunut', 'Lisää nimi ja paikka.')
+    }
+  }
 
   return (
     <View style={styles.container}>
       <TextInput
-      style={styles.textInput}
-      label='Teen nimi'
-      value={name}
+        style={styles.textInput}
+        label='Teen nimi'
+        onChangeText={text => setTea({ ...tea, name: text })}
+        value={tea.name}
       />
       <TextInput
-      style={styles.textInput}
-      label='Ostopaikka'
-      value={place}
+        style={styles.textInput}
+        label='Ostopaikka'
+        onChangeText={text => setTea({ ...tea, place: text })}
+        value={tea.place}
       />
 
-      <Button mode='contained' icon='content-save'>
+      <Button mode='contained' icon='content-save' onPress={saveItem}>
         Tallenna
       </Button>
-      
+
     </View>
   );
 }
