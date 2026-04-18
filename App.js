f1a5f441581410924388ab586745116de5e323e1
Jenni Muhonen <145614394+jennimuhonen@users.noreply.github.com>
2026-04-18 19:25:59 +0300

    fix: ikonit
    Ikonit alapalkkiin

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { PaperProvider } from 'react-native-paper';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from './Home';
 import AddTea from './AddTea';
 import MyTeaStack from './MyTeaStack';
@@ -9,6 +10,7 @@ import MyTeaStack from './MyTeaStack';
 Lähteet:
 - Kurssin verkkosivusto: https://haagahelia.github.io/mobilecourse/
 - React Native Paper: https://oss.callstack.com/react-native-paper/ (Bottom Navigation)
+- Ikonit: https://icons.expo.fyi/Index
 */
 
 const Tab = createBottomTabNavigator();
@@ -17,7 +19,23 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Koti') {
+                iconName = 'home-outline';
+              } else if (route.name === 'Minun teeni') {
+                iconName = 'leaf-outline';
+              } else if (route.name === 'Lisää tee') {
+                iconName = 'add-circle-outline';
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
           <Tab.Screen name="Koti" component={Home} />
           <Tab.Screen name="Minun teeni" options={{ headerShown: false }} component={MyTeaStack} />
           <Tab.Screen name="Lisää tee" component={AddTea} />
